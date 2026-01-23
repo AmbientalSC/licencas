83f1e7ab6a150ab21b7241f3d65ad8a7fdb5da66
CobaHK <alysson@ambiental.sc>
2026-01-23 13:32:40 -0300

    Updates

--- components/icons/FilterIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const FilterIcon: React.FC<{ className?: string }> = ({ className = 'h-6 w-6' }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2a1 1 0 01-.293.707l-6.414 6.414A1 1 0 0013 13.414V19a1 1 0 01-1.447.894l-4-2A1 1 0 017 17v-3.586a1 1 0 00-.293-.707L3.293 6.707A1 1 0 013 6V4z" />
-  </svg>
-); 
--- components/icons/ImportIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const ImportIcon: React.FC<{ className?: string }> = ({ className = 'h-6 w-6' }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M4 17v2a2 2 0 002 2h12a2 2 0 002-2v-2" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M8 12l4-4m0 0l4 4m-4-4v9" />
-  </svg>
-); 
